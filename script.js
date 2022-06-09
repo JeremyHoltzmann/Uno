341055987f19b9ae64c3ddcc5df6d99cfe156bf6
Jérémy <gg1233@gmail.com>
2022-06-09 16:43:11 +0200

    Code Cleanup

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 
-function mod(n, m)
-{
-    return ((n % m) + m) % m;
-}
-
-
-
+//#region Logique
+//#endregion
 var gameState = {
     numberOfPlayer: 3,
     numberOfCardToGive: 7,
     generateSpecialCard: true,
     players: [],
+
+
     playerHasToPlay: true,
     playerHasToDraw: false,
+    
     lastCardPlayed: [],
 
     drawingDeck: [],
@@ -21,27 +19,19 @@ var gameState = {
     playingDirection: 1,
 }
 
-var possibleColor = ["Rouge", "Bleu", "Vert", "Jaune"];
+var possibleColor = ["rouge", "bleu", "vert", "jaune"];
 
 
 function addPlayer()
 {
-    var playersDom = document.getElementsByClassName("playersHands");
     for(var i = 0; i < gameState.numberOfPlayer; i++)
     {
-        var newPlayerDom = document.createElement("div");
-        newPlayerDom.classList.add("playerHand");
-  
         var p1ayer = {
             playerOrder: i,
             cards: new Array(),
             playableCards: new Array(),
             canPlay: true,
         };
-        newPlayerDom.setAttribute("id", "Player " + i);
-        // A sortir
-        playersDom[0].appendChild(newPlayerDom);
-        // A sortir
         gameState.players.push(p1ayer);
     }
     
@@ -85,17 +75,6 @@ function shuffleDeck()
     return deck;
 }
 
-function renderDrawingDeck()
-{
-    var playedCard = document.querySelector(".drawingDeck");
-
-    var cardDom = document.createElement("p");
-    cardDom.classList.add("card");
-    cardDom.classList.add("verso");
-    playedCard.appendChild(cardDom);
-}
-
-
 function giveCards()
 {
     for (var i = 0; i < gameState.numberOfCardToGive; i++)
@@ -117,27 +96,6 @@ function generateCardsDom()
     }   
 }
 
-function generateCardDom(player)
-{
-    var doms = document.getElementById("Player "+ player.playerOrder);
-    while(doms.firstChild)
-        doms.removeChild(doms.lastChild);
-    document.getElementById("Player "+ player.playerOrder).children = 'null';
-    for (var i = 0; i < player.cards.length; i++)
-    {
-        var cardDom = document.createElement("p");
-        cardDom.classList.add("card");
-        var textNode = document.createTextNode("Couleur : " + player.cards[i].color + " Nombre : " + player.cards[i].number);
-        
-        var cardId = (player.playerOrder * gameState.numberOfCardToGive) + i;
-
-        cardDom.setAttribute("id", cardId);
-        cardDom.appendChild(textNode);
-        document.getElementById("Player "+ player.playerOrder).appendChild(cardDom);
-    }
-}
-
-
 function giveCard(playingDeck)
 {
     var currentDeck = playingDeck;
@@ -151,38 +109,6 @@ function initPlayedCard()
     gameState.lastCardPlayed = [];
     gameState.lastCardPlayed.unshift(gameState.drawingDeck[0]);
     gameState.drawingDeck.splice(0, 1);
-
-    var playedCard = document.querySelector(".playedDeck");
-
-    var cardDom = document.createElement("p");
-    cardDom.classList.add("card");
-    var textNode = document.createTextNode("Couleur : " + gameState.lastCardPlayed[0].color + " Nombre : " + gameState.lastCardPlayed[0].number);
-    cardDom.appendChild(textNode);
-    playedCard.appendChild(cardDom);
-}
-
-function updateRenderPlayedCard()
-{
-    var playedCard = document.querySelector(".playedDeck");
-
-    var cardDom = document.createElement("p");
-    cardDom.classList.add("card");
-    var textNode = document.createTextNode("Couleur : " + gameState.lastCardPlayed[0].color + " Nombre : " + gameState.lastCardPlayed[0].number);
-    cardDom.appendChild(textNode);
-
-    while(playedCard.firstChild)
-        playedCard.removeChild(playedCard.lastChild);
-
-    playedCard.appendChild(cardDom);
-}
-
-function updateCardPlayed(cardDiv)
-{
-    var cardChildren = cardDiv.children[0].children[0];
-    cardDiv.classList.remove(gameState.lastCardPlayed.color);
-    cardDiv.classList.add(gameState.lastCardPlayed[0].color);
-    
-    cardChildren[0].textContent = gameState.lastCardPlayed[0].number;
 }
 
 
@@ -216,6 +142,7 @@ function drawCard(numberOfCardToDraw)
         var lastCardPlayer = gameState.lastCardPlayed.splice(1, gameState.lastCardPlayed.length - 1);
 
         gameState.drawingDeck = lastCardPlayer;
+        shuffleDeck();
     }
     return gameState.drawingDeck.splice(0, numberOfCardToDraw);
 }
@@ -226,21 +153,31 @@ function addClickEventListenerToCard()
 {
     console.log("Player " + gameState.currentPlayerIndex);
     var cards = document.getElementById("Player " + gameState.currentPlayerIndex);
-    console.log("PRE INCR " + gameState.currentPlayerIndex);
     for (var i = 0; i < cards.children.length; i++)
     {
+        //findPlayableCards
+        //Sinon Update DrawDeck
+
         cards.children[i].addEventListener("click", function()
         {
             gameState.currentPlayerIndex += gameState.playingDirection;
 
             gameState.currentPlayerIndex = gameState.currentPlayerIndex % gameState.players.length;
-            console.log(gameState.currentPlayerIndex);
+            if (gameState.currentPlayerIndex == -1)
+            {
+                gameState.currentPlayerIndex = (gameState.currentPlayerIndex * gameState.players.length * -1) - 1;
+            }
+
+            //playCards
+
             render();
         })
     }
-    console.log("PRE INCR " + gameState.currentPlayerIndex);
+    //A refaire en une boucle
     for (var j = 0; j < gameState.players.length; j++)
     {
+        //remove event from drawDeck
+
         if (j == gameState.currentPlayerIndex)
             continue;
         var card = document.getElementById("Player " + gameState.currentPlayerIndex);
@@ -284,13 +221,6 @@ function playerTurn()
         }
         gameState.players[gameState.currentPlayerIndex] = currentPlayer;
     }
-    gameState.currentPlayerIndex += gameState.playingDirection;
-
-    gameState.currentPlayerIndex = gameState.currentPlayerIndex % gameState.players.length;
-    if (gameState.currentPlayerIndex <= -1)
-    {
-        gameState.currentPlayerIndex = gameState.currentPlayerIndex * gameState.players.length * -1;
-    }
     return false;
 }
 
@@ -312,19 +242,6 @@ function render()
            // A sortir
     //updateRenderPlayableCard(currentPlayer.playableCards);
            // A sortir
-}
-
-function updateRenderPlayableCard(cards)
-{
-    console.log("Player " + gameState.currentPlayerIndex);
-    var cards = document.getElementById("Player " + gameState.currentPlayerIndex);
-    for (var i = 0; i < cards.children.length; i++)
-    {
-        cards.children[i].addEventListener("click", function()
-        {
-            console.log(this);
-        })
-    }
 }
 
 async function main()
